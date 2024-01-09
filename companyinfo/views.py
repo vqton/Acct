c118fa74845f5b940f90c068eacf19f42d1adf4f
vqton <vuquangton@outlook.com>
2024-01-09 16:23:46 +0700

    implement custom login

--- companyinfo/views.py
+++ companyinfo/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,HttpResponse
+from django.contrib.auth.decorators import login_required, permission_required
 # Create your views here.
+
+from .forms import CompanyInformationForm
+
+@login_required
+@permission_required('companyinfo.add_companyinformation', 'companyinfo.view_companyinformation')
+def index(request):
+    if request.method == 'POST':
+        form = CompanyInformationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = CompanyInformationForm()
+    return render(request, 'companyinfo/index.html', {'form': form})
--- htktAcct/urls.py
+++ htktAcct/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path("", include("home.urls")),
+    path("companyinfo/", include("companyinfo.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+     path('accounts/login/', LoginView.as_view(), name='login'),
     path("admin/", admin.site.urls),
 ]
