25e99c5b3cc3cb8fdbe2498348f3b07bb1a33ebc
TONvq <vuquangton@outlook.com>
2024-01-14 20:09:49 +0700

    abc

--- htktAcct/settings.py
+++ htktAcct/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "acct",
         "USER": "root",
-        "PASSWORD": "",
+        "PASSWORD": "1",
         "HOST": "127.0.0.1",  # Or an IP Address that your DB is hosted on
         "PORT": "3306",
     }
