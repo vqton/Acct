b45d8d1a6cf1c789c919499f257bb4c74fa5d633
vqton <vuquangton@outlook.com>
2024-01-22 16:27:27 +0700

    Update model COA.

--- COA/models.py
+++ COA/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 
+class AccountType(models.Model):
+    type_id = models.CharField(max_length=1, primary_key=True)
+    type_name = models.CharField(max_length=255)
+
+class AccountGroup(models.Model):
+    group_id = models.CharField(max_length=1, primary_key=True)
+    group_name = models.CharField(max_length=255)
 
 class Account(models.Model):
-    account_code = models.IntegerField()
-    account_name = models.CharField(max_length=50)
-    account_type = models.IntegerField()
-    account_group = models.IntegerField()
-    account_content = models.CharField(max_length=255)
-    accounting_method = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.account_name
-
-   
+    account_id = models.CharField(max_length=4, primary_key=True)
+    account_name = models.CharField(max_length=255)
+    type_id = models.ForeignKey(AccountType, on_delete=models.CASCADE)
+    group_id = models.ForeignKey(AccountGroup, on_delete=models.CASCADE)
+    parent_account_id = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
