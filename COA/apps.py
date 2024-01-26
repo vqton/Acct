a606d672ddcc4f311b067b721102c92116a15769
vqton <vuquangton@outlook.com>
2024-01-26 14:06:05 +0700

    remove COA app.

--- COA/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CoaConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "COA"
--- htktAcct/settings.py
+++ htktAcct/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "acct",
         "USER": "root",
-        "PASSWORD": "1",
+        "PASSWORD": "",
         "HOST": "127.0.0.1",  # Or an IP Address that your DB is hosted on
         "PORT": "3306",
     }
