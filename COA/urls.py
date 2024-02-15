e45df304777fa992056e5a81fdef2476e0107e4d
vqton <vuquangton@outlook.com>
2024-02-15 09:08:04 +0700

    ABC

--- COA/urls.py
+++ COA/urls.py
@@ -1,1 +1,11 @@
-urlpatterns=[]
+from django.urls import path
+
+app_name = 'coa'
+
+urlpatterns = [
+    path('accounts/', AccountListView.as_view(), name='account_list'),
+    path('accounts/create/', AccountCreateView.as_view(), name='account_create'),
+    path('accounts/<int:pk>/', AccountDetailView.as_view(), name='account_detail'),
+    path('accounts/<int:pk>/update/', AccountUpdateView.as_view(), name='account_update'),
+    path('accounts/<int:pk>/delete/', AccountDeleteView.as_view(), name='account_delete'),
+]
--- COA/views.py
+++ COA/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from .models import Account
+from django.views.generic import ListView,CreateView,DetailView, UpdateView, DeleteView
+class AccountListView(ListView):
+    model = Account
+    template_name = 'coa/account_list.html'
 
-# Create your views here.
+class AccountCreateView(CreateView):
+    model = Account
+    template_name = 'coa/account_create.html'
+    fields = ['code', 'name', 'level', 'account_type', 'description', 'opening_balance', 'debit_only', 'parent_account']
+
+class AccountDetailView(DetailView):
+    model = Account
+    template_name = 'coa/account_detail.html'
+
+class AccountUpdateView(UpdateView):
+    model = Account
+    template_name = 'coa/account_update.html'
+    fields = ['code', 'name', 'level', 'account_type', 'description', 'opening_balance', 'debit_only', 'parent_account']
+
+class AccountDeleteView(DeleteView):
+    model = Account
+    template_name = 'coa/account_delete.html'
+    success_url = reverse_lazy('coa:account_list')
